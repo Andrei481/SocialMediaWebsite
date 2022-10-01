ca6dc07970e286275fdf523bb32c00c5cda0d05d
Andrei J <andrei.joldea@student.upt.ro>
2022-10-01 19:47:14 +0300

    added search bar

--- base/views.py
+++ base/views.py
@@ -1,4 +1,4 @@
-from multiprocessing import context
+from django.db.models import Q
 from django.shortcuts import render, redirect
 from .models import Room, Topic
 from .forms import RoomForm
@@ -12,9 +12,15 @@ from .forms import RoomForm
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
-    rooms = Room.objects.filter(topic__name__icontains=q) 
+    
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+        ) 
     topics = Topic.objects.filter()
-    context = {'rooms':rooms, 'topics': topics}
+    room_count = rooms.count()
+    context = {'rooms':rooms, 'topics': topics, 'room_count': room_count}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
