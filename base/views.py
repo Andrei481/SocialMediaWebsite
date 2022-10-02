42e223f004babb70884d824a1b4002c595309380
Andrei J <andrei.joldea@student.upt.ro>
2022-10-02 19:11:49 +0300

    display room conversation

--- base/views.py
+++ base/views.py
@@ -50,6 +50,7 @@ def registerPage(request):
     
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
+        
         if form.is_valid():
             user = form.save(commit=False)
             user.username = user.username.lower()
@@ -77,18 +78,21 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    context = {'room':room}
+    room_messages = room.message_set.all().order_by('-created')
+    context = {'room': room, 'room_messages': room_messages}
     
     return render(request, 'base/room.html', context)
 
 @login_required(login_url='/login')
 def createRoom(request):
     form = RoomForm()
+    
     if request.method == 'POST':
         form = RoomForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('home')
+        
     context = {'form': form}
     return render(request, 'base/room_form.html', context)
 
