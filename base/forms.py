b1e306087553adbcdbb1f9b17e9ac8efecc06e22
Andrei J <andrei.joldea@student.upt.ro>
2022-10-11 09:35:54 +0300

    styled register form

--- base/forms.py
+++ base/forms.py
@@ -1,6 +1,22 @@
+import email
 from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from .models import Room, Topic
+from django import forms
 
+
+class RegisterUserForm(UserCreationForm):
+    username = forms.CharField(max_length=50)
+    password = forms.CharField(max_length=50)
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
+    first_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
+    last_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
+    
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password')
+    
 class RoomForm(ModelForm):
     class Meta:
         model = Room
